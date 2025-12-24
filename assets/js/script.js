095d1eb9598057a22af5a883ce6868498bb83b39
nawazdevx <nawazdevx@gmail.com>
2025-12-24 08:08:22 +0500

    Add mobile layout and functionality

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ *  ────── Add event on element
+ */
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ *  ────── Navbar toggle for mobile
+ */
+const navbar = document.querySelector('[data-navbar]');
+const navbarLinks = document.querySelectorAll('[data-nav-link]');
+const navToggler = document.querySelector('[data-nav-toggler]');
+
+const toggleNavbar = function () {
+  navbar.classList.toggle('active');
+  navToggler.classList.toggle('active');
+  document.body.classList.toggle('active');
+};
+
+addEventOnElem(navToggler, 'click', toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove('active');
+  navToggler.classList.remove('active');
+  document.body.classList.remove('active');
+};
+
+addEventOnElem(navbarLinks, 'click', closeNavbar);
+
+/**
+ *  ────── Header active
+ */
+const header = document.querySelector('[data-header]');
+
+const activeHeader = function () {
+  if (window.scrollY > 300) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+};
+
+addEventOnElem(window, 'scroll', activeHeader);
+
+/**
+ *  ────── Toggle active on add to fav
+ */
+const addToFavBtns = document.querySelectorAll('[data-add-to-fav]');
+
+const toggleActive = function () {
+  this.classList.toggle('active');
+};
+
+addEventOnElem(addToFavBtns, 'click', toggleActive);
+
+/**
+ *  ────── Scroll revreal effect
+ */
+const sections = document.querySelectorAll('[data-section]');
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.5) {
+      sections[i].classList.add('active');
+    } else {
+      sections[i].classList.remove('active');
+    }
+  }
+};
+
+scrollReveal();
+
+addEventOnElem(window, 'scroll', scrollReveal);
